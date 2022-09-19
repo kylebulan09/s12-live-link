5b95068c697d57b06eadc83deb74a38c7dc74097
jbulan9 <jbulan@vertere-gs.com>
2022-09-19 14:29:30 +0800

    s12 activity

---
+++ index.js
@@ -0,0 +1,48 @@
+let firstName = 'John';
+console.log("First Name: " + firstName);
+
+let lastName = 'Smith';
+console.log("Last Name: " + lastName);
+
+let age = 30;
+console.log("Age: " + age);
+
+let hobbies = ['Biking','Mountain Climbing','Swimming'];
+console.log("Hobbies:");
+console.log(hobbies);
+
+let workAddress = {
+	houseNumber: '32',
+	street: 'Washington',
+	city: 'Lincoln',
+	state: 'Nebraska'};
+console.log("Work Address:");
+console.log(workAddress);
+
+let fullName = 'Steve Rogers';
+console.log("My full name is: " + fullName);
+
+let currentAge = 40;
+console.log("My current age is: " + currentAge);
+
+let friends = ['Tony', 'Bruce', 'Thor', 'Natasha', 'Clint', 'Nick'];
+console.log("My friends are:");
+console.log(friends);
+
+let profile = {
+	username: 'captain_america',
+	fullName: 'Steve Rogers',
+	age: 40,
+	isActive: false};
+console.log("My Full Profile:");
+console.log(profile);
+
+let bestfriend = 'Bucky Barnes'
+console.log("My bestfriend is: " + bestfriend)
+
+let continent = 'Artic Ocean'
+console.log("I was found frozen in: " + continent)
+
+
+
+
